b484f9562a0931efd65c1ebe75c6442ebd07285c
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-21 17:02:41 +0200

    Add TheatreHall model

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=128)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return self.name
