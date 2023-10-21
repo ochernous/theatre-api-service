49f61bbb902c135dca72aa05d75b362df5650b68
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-21 22:55:23 +0200

    Create urls.py in theatre app, add router for TheatreHallViewSet

---
+++ theatre/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from theatre.views import TheatreHallViewSet
+
+router = routers.DefaultRouter()
+router.register("theatre_halls", TheatreHallViewSet)
+
+urlpatterns = [
+    path("", include(router.urls))
+]
+
+app_name = "theatre"
