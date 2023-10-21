b8b39051e86c53eb6a5ef5f6b790565ad0ff74cd
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-22 00:09:45 +0200

    Add model Genre

--- theatre/models.py
+++ theatre/models.py
@@ -16,3 +16,10 @@ class Actor(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return self.name
