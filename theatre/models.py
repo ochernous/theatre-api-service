27dafc838681f65e625641a66b1832ed76d4ab60
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-22 00:01:11 +0200

    Add model Actor

--- theatre/models.py
+++ theatre/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 
 class TheatreHall(models.Model):
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=127)
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
 
     def __str__(self):
         return self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
