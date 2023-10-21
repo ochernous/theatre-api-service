805f0aa8ad813b85e7c8a1bdb32e61051a0587c8
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-22 01:19:22 +0200

    Update urls.py - add routes for TicketViewSet

--- theatre/urls.py
+++ theatre/urls.py
@@ -8,6 +8,7 @@ from theatre.views import (
     PlayViewSet,
     PerformanceViewSet,
     ReservationViewSet,
+    TicketViewSet,
 )
 
 router = routers.DefaultRouter()
@@ -17,6 +18,7 @@ router.register("genres", GenreViewSet)
 router.register("plays", PlayViewSet)
 router.register("performances", PerformanceViewSet)
 router.register("reservations", ReservationViewSet)
+router.register("tickets", TicketViewSet)
 
 urlpatterns = [
     path("", include(router.urls))
