a917c5eca8c3e743a2e83e55421652db7b4e424d
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-22 00:02:51 +0200

    Add ActorViewSet

--- theatre/views.py
+++ theatre/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
-from theatre.models import TheatreHall
-from theatre.serializers import TheatreHallSerializer
+from theatre.models import TheatreHall, Actor
+from theatre.serializers import TheatreHallSerializer, ActorSerializer
 
 
 class TheatreHallViewSet(viewsets.ModelViewSet):
     queryset = TheatreHall.objects.all()
     serializer_class = TheatreHallSerializer
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
