642b14231ac58597cceb4b24776380de5ec100ad
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-22 00:02:35 +0200

    Add serializer ActorSerializer

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from theatre.models import TheatreHall
+from theatre.models import TheatreHall, Actor
 
 
 class TheatreHallSerializer(serializers.ModelSerializer):
     class Meta:
         model = TheatreHall
         fields = ("id", "name", "rows", "seats_in_row")
+
+
+class ActorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = ("id", "first_name", "last_name")
