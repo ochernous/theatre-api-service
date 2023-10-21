09ff4495af727fed87249a90aa9f0622be8f972b
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-22 01:20:55 +0200

    Update admin.py - register TheatreHall, Actor, Genre, Play, Performance, Reservation and Ticket models

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 
-from theatre.models import TheatreHall
+from theatre.models import (
+    TheatreHall,
+    Actor,
+    Genre,
+    Play,
+    Performance,
+    Reservation,
+    Ticket
+)
 
 admin.site.register(TheatreHall)
+admin.site.register(Actor)
+admin.site.register(Genre)
+admin.site.register(Play)
+admin.site.register(Performance)
+admin.site.register(Reservation)
+admin.site.register(Ticket)
