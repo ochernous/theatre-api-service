45b892c0f2458a9452be4c83aed0ba73b16a6ad0
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-22 11:59:34 +0200

    Update models Reservation and Performance - add related names

--- theatre/models.py
+++ theatre/models.py
@@ -46,8 +46,8 @@ class Play(models.Model):
 
 class Performance(models.Model):
     show_time = models.DateTimeField()
-    play = models.ForeignKey(Play, on_delete=models.CASCADE)
-    theatre_hall = models.ForeignKey(TheatreHall, on_delete=models.CASCADE)
+    play = models.ForeignKey(Play, on_delete=models.CASCADE, related_name="performances")
+    theatre_hall = models.ForeignKey(TheatreHall, on_delete=models.CASCADE, related_name="performances")
 
     def __str__(self):
         return f"{self.play.title} {self.show_time}"
@@ -55,7 +55,7 @@ class Performance(models.Model):
 
 class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="reservations")
 
     def __str__(self):
         return self.created_at
