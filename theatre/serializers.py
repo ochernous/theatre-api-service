53dc9e341fede4b95051360ef668faff8e8c030e
Oleh Chernous <chernous.oleh@gmail.com>
2023-10-22 11:35:37 +0200

    Update TheatreHallSerializer and Actor serializer - add property fields

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -13,13 +13,13 @@ from theatre.models import (
 class TheatreHallSerializer(serializers.ModelSerializer):
     class Meta:
         model = TheatreHall
-        fields = ("id", "name", "rows", "seats_in_row")
+        fields = ("id", "name", "rows", "seats_in_row", "capacity")
 
 
 class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
-        fields = ("id", "first_name", "last_name")
+        fields = ("id", "first_name", "last_name", "full_name")
 
 
 class GenreSerializer(serializers.ModelSerializer):
